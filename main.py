0f263bcadef77fbfebd2d165894684dc29f660b1
onyxmur <vroeushin@gmail.com>
2025-02-26 02:14:58 +0500

    add drag and drop some cards

--- main.py
+++ main.py
@@ -123,10 +123,12 @@ if __name__ == '__main__':
     window.show()
     empty_cards, columns = distribute_cards(deck)
     selected_card = None
+    selected_cards = []
     start_x, start_y = None, None
     offset_x = 0
     offset_y = 0
     dragging = False
+    flag = False
 
     foundations = []
     for i in range(4):
@@ -151,15 +153,23 @@ if __name__ == '__main__':
 
             elif event.type == pygame.MOUSEBUTTONDOWN:  # начало перетаскивания
                 selected_card = None
+                selected_cards = []
                 mouse_pos = event.pos
                 for column in columns:
-                    for card in column:
+                    for card in reversed(column):
                         if card.is_clicked(mouse_pos) and card.face_up:
                             selected_card = card
+                            if columns[selected_card.column].index(selected_card) != len(
+                                    columns[selected_card.column]) - 1:
+                                flag = True
+                                selected_cards = columns[selected_card.column][
+                                                 columns[selected_card.column].index(selected_card):]
+
                             start_x, start_y = selected_card.x, selected_card.y
                             dragging = True
                             offset_x = selected_card.x - event.pos[0]
                             offset_y = selected_card.y - event.pos[1]
+                            break
 
             elif event.type == pygame.MOUSEBUTTONUP and selected_card is not None:  # конец перетаскивания
                 sp = []
@@ -186,14 +196,28 @@ if __name__ == '__main__':
                     start_column = selected_card.column
                     new_column = \
                         [i for i in range(len(empty_sp)) if empty_sp[i] != True and i != selected_card.column][0]
+                    if len(columns[selected_card.column]) > 0:
+                        columns[selected_card.column][
+                            columns[selected_card.column].index(selected_card) - 1].face_up = True
 
                     columns[new_column].append(selected_card)
                     columns[selected_card.column].remove(selected_card)
-                    selected_card.column = new_column
+
                     if len(columns[start_column]) > 0:
                         columns[start_column][-1].face_up = True
+                    selected_card.column = new_column
                     selected_card.x = empty_cards[new_column].x
                     selected_card.y = COLUMNS_START_Y + (len(columns[new_column]) - 1) * CARD_OFFSET_Y
+                    if flag:
+                        j = 0
+                        for i in selected_cards[1:]:
+                            columns[new_column].append(i)
+                            columns[i.column].remove(i)
+                            i.column = new_column
+                            i.x = columns[new_column][0].x
+                            i.y = selected_card.y + CARD_OFFSET_Y * (1 + j)
+                            j += 1
+
                 elif len_sp > 1:
                     start_column = selected_card.column
                     clll = [i for i in range(len(spp)) if spp[i] == True and i != selected_card.column]
@@ -203,26 +227,42 @@ if __name__ == '__main__':
 
                     # Проверка правил перемещения
                     if (selected_card.is_opposite_color(target_card) and selected_card.is_one_rank_higher(
-                            target_card)):
+                            target_card) and target_card.rank != 14):
+                        if len(columns[selected_card.column]) > 0:
+                            columns[selected_card.column][
+                                columns[selected_card.column].index(selected_card) - 1].face_up = True
+
                         columns[new_column].append(selected_card)
                         columns[selected_card.column].remove(selected_card)
-
-                        if len(columns[selected_card.column]) > 0:
-                            columns[selected_card.column][-1].face_up = True
 
                         selected_card.column = new_column
                         selected_card.x = columns[new_column][0].x
                         selected_card.y = COLUMNS_START_Y + (len(columns[new_column]) - 1) * CARD_OFFSET_Y
+                        if flag:
+                            j = 0
+                            for i in selected_cards[1:]:
+                                columns[new_column].append(i)
+                                columns[i.column].remove(i)
+                                i.column = new_column
+                                i.x = columns[new_column][0].x
+                                i.y = target_card.y + CARD_OFFSET_Y + CARD_OFFSET_Y * (1 + j)
+                                j += 1
+
+
+
                     else:
                         selected_card.x = start_x
                         selected_card.y = start_y
-
-
-
+                        for j, i in enumerate(selected_cards[1:]):
+                            i.x = start_x
+                            i.y = start_y + CARD_OFFSET_Y * (j + 1)
 
                 else:
                     selected_card.x = start_x
                     selected_card.y = start_y
+                    for j, i in enumerate(selected_cards[1:]):
+                        i.x = start_x
+                        i.y = start_y + CARD_OFFSET_Y * (j + 1)
                 dragging = False
 
 
@@ -231,10 +271,18 @@ if __name__ == '__main__':
                     selected_card.x = event.pos[0] + offset_x
                     selected_card.y = event.pos[1] + offset_y
 
+                    for j, i in enumerate(selected_cards[1:]):
+                        i.x = event.pos[0] + offset_x
+                        i.y = event.pos[1] + offset_y + (j + 1) * CARD_OFFSET_Y
+
         if selected_card is not None:
             selected_card.rect.x = selected_card.x
             selected_card.rect.y = selected_card.y
             screen.blit(selected_card.image, selected_card.rect.topleft)
+            for i in selected_cards[1:]:
+                i.rect.x = i.x
+                i.rect.y = i.y
+                screen.blit(i.image, i.rect.topleft)
 
         pygame.display.flip()
 
